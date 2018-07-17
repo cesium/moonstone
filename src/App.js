4febf2db0a760cfaca54f635c9e50c2d3abf0f85
Eduardo Jorge <herulume@protonmail.com>
2018-07-17 17:30:33 +0100

    Add basic routing (#10)
    * Add react-router-dom as a dependency;
    * Using BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from './components/Home';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <Header />
-        <h1>SEI 2019</h1>
-        <Footer />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
     );
   }
 };
---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./index.css"
+
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Home = () => (
+      <div>
+        <Header />
+          <h1>SEI 2019</h1>
+        <Footer />
+      </div>
+);
+
+export default Home;
