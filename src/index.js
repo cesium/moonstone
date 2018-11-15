dae5dfdad1992601d8806bdf6aeeb56201c5c791
puthz <antoniopedromb@gmail.com>
2018-11-15 19:39:48 +0000

    Resolved merge issues

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import IndexPage from "./pages/index";
 import Layout from "./layouts/index";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
-import "./index.css";
-
-const Root = () => (
-  <Layout>
+ import "./index.css";
+ 
+ const Root = () => (
+   <Layout>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={IndexPage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register/:id" component={Register} />
         {/* <Route component={Error404} /> */}
       </Switch>
     </BrowserRouter>
