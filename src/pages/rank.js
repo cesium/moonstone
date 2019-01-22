35e883176d36305faa60b431eaf66318fe2eef8f
Mendess2526 <pedro.mendes.26@gmail.com>
2019-01-22 14:25:48 +0000

    Add sidebar

--- src/pages/rank.js
+++ src/pages/rank.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Box, Table, TableBody, TableCell, TableHeader, TableRow,
   Text } from "grommet";
 import axios from "axios";
+import userInfo from "../containers/userInfo";
 
 const COLUMNS = ["Rank", "Name", "Badges"];
 
@@ -9,18 +10,40 @@ class RankPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      error: ""
     };
   }
 
   handleLeaderBoard(users) {
+    console.log(this.props);
     if (users.data.hasOwnProperty("data")) {
-      this.setState({users: users.data.data});
+      //<TableRow color={u.id === this.props.id ? "accent-2" : "accent-1"} key={i}>
+      let board = users.data.data.map((u, i) => (
+        <TableRow key={i}>
+          <TableCell key={COLUMNS[0]}>
+              <Text>{i}</Text>
+          </TableCell>
+          <TableCell key={COLUMNS[1]}>
+            <Text>{u.nickname}</Text>
+          </TableCell>
+          <TableCell key={COLUMNS[2]}>
+            <Text>{u.badges.length}</Text>
+          </TableCell>
+        </TableRow>
+      ));
+      this.setState({users: board});
     }
   }
 
   handleError(error) {
-    window.location.pathname = "/login";
+    if (error.response
+      && error.response.data.hasOwnProperty("error")
+      && error.response.data.error === "unauthenticated")
+    {
+      window.location.pathname = "/login";
+    }
+    this.setState({ error: "Network Error" });
   }
 
   componentDidMount() {
@@ -50,25 +73,16 @@ class RankPage extends Component {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {this.state.users.map((u, i) => (
-              <TableRow key={i}>
-                <TableCell key={COLUMNS[0]}>
-                  <Text>{i}</Text>
-                </TableCell>
-                <TableCell key={COLUMNS[1]}>
-                  <Text>{u.nickname}</Text>
-                </TableCell>
-                <TableCell key={COLUMNS[2]}>
-                  <Text>{u.badges.length}</Text>
-                </TableCell>
-              </TableRow>
-            ))}
+            {this.state.users}
           </TableBody>
         </Table>
+        <Box>
+          <Text color="status-critical">{this.state.error}</Text>
+        </Box>
       </Box>
     );
   }
 }
 
-export default RankPage;
+export default userInfo(RankPage);
 
