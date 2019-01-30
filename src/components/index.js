9eda29ef64ea1910a8573536c2c7e7600783d5e3
Mendess2526 <pedro.mendes.26@gmail.com>
2019-01-30 18:40:03 +0000

    Add dependency

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Account from "./Account";
 import Welcome from "./Welcome";
+import ImageUpload from "./ImageUpload";
 
-export { Footer, Header, Account, Welcome };
+export { Footer, Header, Account, Welcome, ImageUpload };
 
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
-import {Box, Image, Text, Heading, RoutedButton, InfiniteScroll} from "grommet";
+import {Box, Image, Text, Heading, RoutedButton, InfiniteScroll, FormField,
+  Button, TextInput} from "grommet";
 import axios from 'axios';
 import userInfo from "../containers/userInfo.js";
+import ImageUpload from "../components/ImageUpload/index.js";
+import {Edit, Checkmark} from "grommet-icons";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {},
-      error: ""
+      error: "",
+      edit_username: false,
+      new_username: "",
+      edit_avatar: false
     };
+    this.swapEditUsername = this.swapEditUsername.bind(this);
+    this.swapEditAvatar = this.swapEditAvatar.bind(this);
   }
 
-  handleBadgesResponse(res) {
+  handleUserResponse(res) {
     if (res.data.hasOwnProperty("data")) {
       this.setState({user: res.data.data, error: ""});
     }
@@ -29,6 +37,33 @@ class Profile extends Component {
     this.setState({ error: "Network Error" });
   }
 
+  swapEditUsername() {
+    this.setState({edit_avatar: false, edit_username: !this.state.edit_username});
+  }
+
+  swapEditAvatar() {
+    this.setState({edit_username: false, edit_avatar: !this.state.edit_avatar});
+  }
+
+  onChangeUsername(event) {
+    this.setState({new_username: event.target.value});
+  }
+
+  editUsername(event) {
+    const api_endpoint =
+      process.env.REACT_APP_ENDPOINT
+      + process.env.REACT_APP_API_ATTENDEES
+      + this.props.user.id;
+    let config = {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+      }
+    };
+    axios.post(api_endpoint, config)
+      .then(res => this.handleUserResponse(res))
+      .catch(error => this.handleErrorResponse(error));
+  }
+
   componentDidMount() {
     const api_endpoint =
       process.env.REACT_APP_ENDPOINT
@@ -40,7 +75,7 @@ class Profile extends Component {
       }
     };
     axios.get(api_endpoint, config)
-      .then(res => this.handleBadgesResponse(res))
+      .then(res => this.handleUserResponse(res))
       .catch(error => this.handleErrorResponse(error));
   }
 
@@ -50,13 +85,44 @@ class Profile extends Component {
   }
 
   render() {
+    const placeholder = "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.newdesignfile.com%2Fpostpic%2F2013%2F05%2Fgeneric-user-icon-windows_166129.png&f=1";
     return (
       <Box>
         {this.state.error === "" ?
             <Box pad={{ horizontal: "medium", bottom: "medium", top: "medium" }} gap="medium">
-              <Heading level="1" alignSelf="center">{this.state.user.nickname}</Heading>
+              {this.state.edit_username ?
+                  <Box direction="row" justify="center">
+                    <FormField>
+                      <TextInput
+                        size="xlarge"
+                        placeholder={this.state.user.nickname}
+                        onChange={this.updateNewUsername}
+                      />
+                    </FormField>
+                    <Button
+                      type="submit"
+                      icon={<Edit />}
+                      color="brand"
+                      onClick={this.swapEditUsername}
+                    />
+                    <Button
+                      icon={<Checkmark />}
+                      onClick={this.editUsername}
+                    />
+                  </Box>
+                  :
+                  <Box justify="center" direction="row">
+                    <Heading level="1" alignSelf="center">{this.state.user.nickname}</Heading>
+                    <Button icon={<Edit />} border="" onClick={this.swapEditUsername} />
+                  </Box>
+              }
               <Box margin="small" align="center" gap="medium">
-                <Image src={this.state.user.avatar}/>
+                <Image fit="contain" src={placeholder/*this.state.user.avatar*/}/>
+                <ImageUpload
+                  id={this.props.user.id}
+                  success={s => console.log(s)}
+                  error={e => console.log(e)}
+                />
               </Box>
               <Heading level="2" alignSelf="center">Badges</Heading>
               <Box
