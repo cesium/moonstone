bc141b6821b162e2c1370409b334a7415342fead
Pedro Barbosa <14206093+apedrob@users.noreply.github.com>
2019-02-02 23:37:18 +0000

    Login page with moonstone logo (#42)

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,92 +1,22 @@
 import React, { Component } from "react";
-import { Box, Button, FormField, Text, TextInput } from "grommet";
-import axios from "axios";
+import {Box} from "grommet";
+import {Login, Welcome} from "../components";
+
+var state = {
+  isLogin: true
+};
 
 class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      error: ""
-    };
-    this.login = this.login.bind(this);
-  }
-
-  onChangeEmail = event => {
-    this.setState({ email: event.target.value });
-  };
-
-  onChangePassword = event => {
-    this.setState({ password: event.target.value });
-  };
-
-  handleLoginResponse(res) {
-    if (res.data.hasOwnProperty("jwt")) {
-      this.setState({ error: "" });
-      localStorage.jwt = res.data.jwt;
-      window.location.pathname = "/";
-    } else {
-      this.setState({ error: "Login error" });
-    }
-  }
-
-  handleErrorResponse(res) {
-    if (res.response) {
-      this.setState({ error: "Invalid email or password" });
-    } else {
-      this.setState({ error: "Login error" });
-    }
-  }
-
-  getErrorText() {
-    return this.state.error;
-  }
-
-  login() {
-    const api_endpoint =
-      process.env.REACT_APP_ENDPOINT + process.env.REACT_APP_API_AUTH_SIGN_IN;
-    axios
-      .post(api_endpoint, {
-        email: this.state.email,
-        password: this.state.password
-      })
-      .then(res => this.handleLoginResponse(res))
-      .catch(res => this.handleErrorResponse(res));
+  openLogin() {
+    state.isLogin = !state.isLogin;
   }
 
   render() {
     return (
-      <Box justify="center" align="center" pad="xlarge" gap="medium">
-        <Box alignSelf="center">
-          <FormField label="Email">
-            <TextInput
-              placeholder="foo@bar.com"
-              size="xlarge"
-              onChange={e => this.onChangeEmail(e)}
-            />
-          </FormField>
-
-          <FormField label="Password">
-            <TextInput
-              type="password"
-              placeholder="********"
-              size="xlarge"
-              onChange={e => this.onChangePassword(e)}
-            />
-          </FormField>
-
-          <Button
-            type="submit"
-            label="Login"
-            color="brand"
-            primary={true}
-            onClick={this.login}
-          />
-        </Box>
-
-        <Box>
-          <Text color="status-critical">{this.getErrorText()}</Text>
+      <Box background="light-1" fill="vertical">
+        <Box margin={{ left: "medium", right:"medium", top: "medium", bottom: "small" }}>
+          <Welcome size="small"/>
+          <Login />
         </Box>
       </Box>
     );
@@ -94,4 +24,3 @@ class LoginPage extends Component {
 }
 
 export default LoginPage;
-
