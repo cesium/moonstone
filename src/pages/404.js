df1e5b673a3045c11ee412b804400a05eabab4e7
Mendess2526 <pedro.mendes.26@gmail.com>
2019-02-02 01:43:30 +0000

    Improve 404 look

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,8 @@ class NoMatch extends Component {
   render() {
     return (
       <Box fill={true} align="center" pad={{top: "large", horizontal: "medium"}} gap="large" width="medium" >
-        <Heading textAlign="center" level="1">404, go <RoutedButton path="/"><Heading color="brand">home</Heading></RoutedButton> you're drunk!</Heading>
+        <Heading textAlign="center" level="1" size="xlarge" color="dark-4">404</Heading>
+        <Heading textAlign="center" level="1">Go <RoutedButton path="/"><Heading color="brand" level="1">home</Heading></RoutedButton> you're drunk!</Heading>
         <Box>
           <Image src={meme} width="auto" height="auto" fit="contain"/>
         </Box>
