89914773a843077b4ffb66e35910beaeb1f420cb
Nelson Estevão <nelsonmestevao@gmail.com>
2019-02-03 07:02:11 +0000

    Update 404 page (#48)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
-import {Box, Image, Heading, RoutedButton} from "grommet";
-import meme from "../images/404.jpg";
+import React, { Component } from 'react';
+import { Box, Image, Heading, RoutedButton, Paragraph } from "grommet";
+import mascote from "../images/mascot.png";
 
 class NoMatch extends Component {
   render() {
     return (
-      <Box fill={true} align="center" pad={{top: "large", horizontal: "medium"}} gap="large" width="medium" >
-        <Heading textAlign="center" level="1" size="xlarge" color="dark-4">404</Heading>
-        <Heading textAlign="center" level="1">Go <RoutedButton path="/"><Heading color="brand" level="1">home</Heading></RoutedButton> you're drunk!</Heading>
+      <Box fill={true} align="center" pad={{ top: "large", horizontal: "medium" }} gap="large" width="medium" >
+        <Heading level="1" size="xlarge" color="dark-4">Oops!</Heading>
+        <Heading level="3">Error code: 404</Heading>
+        <Paragraph margin="none">
+          Mistakes are the portals of discovery. But we'd rather you use our <RoutedButton path="/"><Heading color="brand" level="4">homepage</Heading></RoutedButton>.
+        </Paragraph>
         <Box>
-          <Image src={meme} width="auto" height="auto" fit="contain"/>
+          <Image src={mascote} width="auto" height="auto" fit="contain" />
         </Box>
-      </Box>);
+      </Box>
+    );
   }
 }
 
