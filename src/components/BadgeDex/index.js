1387c908c823fb3b3dc8f92eb0840e998d50ab58
Pedro Mendes <pedro.mendes.26@gmail.com>
2019-02-04 20:54:15 +0000

    Add badge tags to hidden and secret badges (#71)

--- src/components/BadgeDex/index.js
+++ src/components/BadgeDex/index.js
@@ -7,10 +7,10 @@ class BadgeDex extends Component {
   render() {
     return (
       <Box
-          pad={{ horizontal: "medium", bottom: "medium" }}
-          justify="center"
-          direction="row"
-          wrap={true}
+        pad={{ horizontal: "medium", bottom: "medium" }}
+        justify="center"
+        direction="row"
+        wrap={true}
       >
         {this.props.badges.map((b, i) => {
           const avatar = b.avatar.includes("missing") ? badge_missing : b.avatar;
@@ -27,11 +27,15 @@ class BadgeDex extends Component {
                   width="150em"
                   src={avatar}
                 />
-                <Text
-                  color={collected ? "dark-4" : "light-4" }
-                >
-                  {"#" + ('000' + b.id).slice(-3)}
-                </Text>
+                <Box direction="collumn" gap="xsmall">
+                  <Text
+                    color={collected ? "dark-4" : "light-4" }
+                  >
+                    {"#" + ('000' + b.id).slice(-3)}
+                  </Text>
+                  {b.type === 0 ? <Text color="accent-1">Joke</Text> : null}
+                  {b.type === 1 ? <Text color="accent-1">Secret</Text> : null}
+                </Box>
                 <Text
                   color={collected ? "" : "light-4" }
                   textAlign="center"
