b72781dfe919ac3c24433e7f9fca6ab612698e43
Mendess2526 <pedro.mendes.26@gmail.com>
2019-02-04 17:58:17 +0000

    Add badge tags to hidden and secret badges

--- src/components/BadgeDex/index.js
+++ src/components/BadgeDex/index.js
@@ -27,11 +27,15 @@ class BadgeDex extends Component {
                   width="150em"
                   src={avatar}
                 />
+                <Box direction="collumn" gap="xsmall">
                 <Text
                   color={collected ? "dark-4" : "light-4" }
                 >
                   {"#" + ('000' + b.id).slice(-3)}
                 </Text>
+                {b.type === 0 ? <Text color="accent-1">Joke</Text> : null}
+                {b.type === 1 ? <Text color="accent-1">Secret</Text> : null}
+                </Box>
                 <Text
                   color={collected ? "" : "light-4" }
                   textAlign="center"
